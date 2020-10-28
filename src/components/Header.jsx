ba0b247a51db81f8d66804b6433093ba4b57c672
harahorinev <harahorinev@gmail.com>
2020-10-28 08:58:41 +0700

    Залили файл на сервер

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className='header'>
+        <img src='https://image.shutterstock.com/image-vector/dots-letter-c-logo-design-260nw-551769190.jpg'></img>
+    </header>
+    )
+}
+export default Header;
