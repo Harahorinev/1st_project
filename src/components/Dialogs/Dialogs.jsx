d8244d72a1e7fb218bd0787a932f5b8e557fab3c
harahorinev <harahorinev@gmail.com>
2020-12-02 13:04:31 +0700

    added new file src/components/

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
-  return <div className={classes.item}>Dialogs</div>
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.usersDialog}>
+        <div className={classes.dialog + ' ' + classes.active}>1</div>
+        <div className={classes.dialog}>2</div>
+        <div className={classes.dialog}>3</div>
+        <div className={classes.dialog}>4</div>
+        <div className={classes.dialog}>5</div>
+      </div>
+      <div className={classes.chats}>
+        <div className={classes.message}>1</div>
+        <div className={classes.message}>2</div>
+        <div className={classes.message}>3</div>
+        <div className={classes.message}>4</div>
+        <div className={classes.message}>5</div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs
